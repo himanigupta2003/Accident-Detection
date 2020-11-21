7f10316006680d33258f34aa2cde9b1cbeedaa78
mad-skull <da.madskull@gmail.com>
2020-11-21 22:09:10 +0530

    Initial Commit

---
+++ htdocs/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>Accident Detection</title>
+	<link rel="shortcut icon" href="./imgs/favicon.png">
+	<link rel="stylesheet" href="/css/index.css">
+	<style>
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center; 
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
+</head>
+<body>
+
+	<div class="container">
+		<!-- <div class="splash-caution">
+			Accident
+		</div> -->
+		<div class="row page-heading">
+			<div class="col s12">
+				<img src="1.png"  style="100px: 150px;">
+			</div>
+		</div>
+
+		<div id="alert-container">
+			<div class="accident-alert">
+			    <div class="col s12">
+					<h2 >
+					    Accident Alert <span>@ 1:20 pm</span>
+					</h2>
+					<div class="alert-details">
+						<h5>
+							Location: Sector - 37, Gurgaon
+						</h5>
+						<p>
+							You are <b>10 minutes away</b> from the site of the accident.
+						</p>
+						<p>
+							You are <b>5 kilometres away</b> from the site of the accident.
+							<br>
+							<button class="button" type="button" onclick="window.location.href = 's.php';">Click Me!</button>
+
+						</p>
+					</div>
+			    </div>
+			</div>
+
+
+    
+   
+
+</body>
+</html>
